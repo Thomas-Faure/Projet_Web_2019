19784fc7558af0b91a400459441cd8725492278b
Thomas Faure <thomas.faure@etu.univ-amu.fr>
2019-05-08 19:18:56 +0200

    controlleur index fait

--- app/Controllers/Http/IndexController.js
+++ app/Controllers/Http/IndexController.js
@@ -5,8 +5,16 @@ const Database = use('Database')
 
 class IndexController {
     async index ({ view }) {
-        const bid = await Bid.all()
-        return view.render('index',{bids : bid.toJSON()})
+        //const bid = await Bid.all()
+        const bid = await Database
+        .select('bid.name_bid as name_bid',
+        Database.raw('DATE_FORMAT(bid.created_at, "%Y-%m-%d %H:%i") as date'),
+        'users.username as username','category_bid.name_bid as name_cat',
+        'category_bid.color as color')
+        .from('bid')
+        .crossJoin('users', 'bid.user_id', 'users.id')
+        .crossJoin('category_bid', 'bid.category_id', 'category_bid.id')
+        return view.render('index',{bids : bid})
     }
 
     async edit ({ params, request, response, view }) {
