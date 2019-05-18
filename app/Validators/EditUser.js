57f3e99b97b871d9bcd032cff7527935442413d6
Thomas Faure <thomas.faure@etu.univ-amu.fr>
2019-05-18 23:53:52 +0200

    Modification du Validator EditUser

--- app/Validators/EditUser.js
+++ app/Validators/EditUser.js
@@ -3,11 +3,13 @@
 class EditUser {
   get rules () {
     return {
-      'name': 'required|max:80',
-      'username': 'required|max:80',
+      'name': 'required|max:80|regex:^[a-zA-Z0-9-\']+$',
+      'username': 'required|max:80|regex:^[a-zA-Z0-9-\']+$',
       'birthday': 'required',
       'email': 'required|email|max:254',
-      'password': 'min:3|max:60'
+      'password': 'min:3|max:60',
+      'passwordValidation': 'same:password',
+
 
     }
   }
@@ -18,7 +20,9 @@ class EditUser {
       'unique' : ' Il se trouve que cet élement existe deja...',
       'email' : ' Vous devez mettre un email dans ce champ...',
       'max' : 'Nombre de caractères autorisé dépassé',
-      'min' : 'Nombre de caractères doit être supérieur'
+      'min' : 'Nombre de caractères doit être supérieur',
+      'same': 'Le mot de passe ne correspond pas',
+      'regex': 'ce n\'est pas un champ valide'
     }
   }
   async fails(error){
